b946ff2baa5e7c27cae0a6197cb276fba5ef99dc
Михаил Терешин <medvedstwo@gmail.com>
2024-06-25 21:21:13 +0300

    refactor: удаляет лишние строки

--- js/functions.js
+++ js/functions.js
@@ -13,29 +13,3 @@ function checkPalindrome(string) {
 
   return (reverseString === stringNormalize);
 }
-
-// поиск и вывод цифр в строке
-
-function checkStringNumber(string) {
-  let listNumbers;
-
-  if (typeof string === 'string') {
-    listNumbers = parseInt(string, 10);
-    return listNumbers;
-  }
-}
-
-// фукнция проверяет символы в строке и отдает цифры
-// - проверить тип  данных
-
-
-// -- если строка
-// --- перебрать строку
-// --- привести к десятичным числам
-// --- записать каждую цифру в переменную
-
-// -- если число
-// --- перевести в строку
-// --- записать каждую цифру в переменную
-
-// - вернуть строку
