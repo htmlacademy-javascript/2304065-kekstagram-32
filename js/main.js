c5d4100f0f448b9689818b41fd453ff4fbf42974
Михаил Терешин <medvedstwo@gmail.com>
2024-07-06 20:11:17 +0300

    fix: исправляет ошибки линтера

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function getRandomInteger(a, b) {
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+}
 
 function createUniqNumber(minNum, maxNum) {
   const previousNumb = [];
