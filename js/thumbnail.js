b47b8395988d84e9818779383838cf3726de725a
Михаил Терешин <medvedstwo@gmail.com>
2024-07-15 22:39:41 +0300

    feat: добавляет рендер картинок

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,19 +1,19 @@
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureContainer = document.querySelector('.pictures');
+const fragement = document.createDocumentFragment();
 
-const pictureElement = pictureTemplate.cloneNode(true);
+function renderPosts(posts) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  console.log(pictureElement);
+  posts.forEach((item) => {
+    pictureElement.querySelector('.picture__img').src = item.url;
+    pictureElement.querySelector('.picture__img').alt = item.description;
+    pictureElement.querySelector('.picture__comments').textContent = item.comment;
+    pictureElement.querySelector('.picture__likes').textContent = item.likes;
+    fragement.append(item);
+  });
 
+  pictureContainer.append(fragement);
+}
 
-
-// Отобразить фотографии других пользователей.
-
-// Заведите модуль, который будет отвечать за отрисовку миниатюр.
-
-// На основе временных данных для разработки и шаблона #picture создайте DOM-элементы, соответствующие фотографиям, и заполните их данными:
-
-// Адрес изображения url подставьте как атрибут src изображения.
-// Описание изображения description подставьте в атрибут alt изображения.
-// Количество лайков likes выведите в блок .picture__likes.
-// Количество комментариев comments выведите в блок .picture__comments.
-// Отрисуйте сгенерированные DOM-элементы в блок .pictures. Для вставки элементов используйте DocumentFragment.
-
-// Подключите модуль в проект.
+export {renderPosts};
