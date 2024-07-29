4b21cd2b33c96095a2a6963ce20572fb9b62772f
Михаил Терешин <medvedstwo@gmail.com>
2024-07-29 21:30:30 +0300

    feat: копирует обработчик

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -3,6 +3,13 @@ const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
 
+function onDocumentKeydown (evt) {
+  if (isEscapeEvt(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+}
+
 function onImgUploadEditing() {
   showModal();
 }
@@ -10,6 +17,7 @@ function onImgUploadEditing() {
 function showModal() {
   body.classList.add('modal-open');
   imgUploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
 }
 
 imgUploadInput.addEventListener('change', onImgUploadEditing);
