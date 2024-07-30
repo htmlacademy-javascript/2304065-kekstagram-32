664d794e782e744b2d0ed7832761c938680866ef
Михаил Терешин <medvedstwo@gmail.com>
2024-07-30 22:38:43 +0300

    feat: добавляет проверку длины коммента

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -5,21 +5,24 @@ const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
 const imgUploadCancel = imgUploadForm.querySelector('.img-upload__cancel');
+const imgComment = imgUploadForm.querySelector('.text__description')
 
-const pristine = new Pristine(imgUploadForm);
-const validHashtag = /^#[a-zа-яё0-9]{1, 19}/i;
-
-imgUploadForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
-  const isValid = pristine.validate();
-
-  if (isValid) {
-
-    console.log('Можно отправить');
-  } else {
-    console.log('Нетъ');
-  }
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error'
 });
+
+const validHashtag = /^#[a-zа-яё0-9]{1, 19}$/i;
+
+function validateComment(comment) {
+  return comment.length <= 140;
+}
+
+pristine.addValidator(imgComment,
+  validateComment,
+  'Комментарий не должен быть больше 140 символов'
+);
 
 function onDocumentKeydown (evt) {
   if (isEscapeEvt(evt)) {
