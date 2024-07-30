ec4d15d1f01a520a4cb173b1488ef33c44229a4c
Михаил Терешин <medvedstwo@gmail.com>
2024-07-30 20:13:23 +0300

    feat: подключает либу и добавяет обработчик закрытия окна

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -1,7 +1,23 @@
+import { isEscapeEvt } from './utils.js';
+
 const body = document.querySelector('body');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
+const imgUploadCancel = imgUploadForm.querySelector('.img-upload__cancel');
+
+const pristine = new Pristine(imgUploadForm);
+
+imgUploadForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+
+  if (isValid) {
+    console.log('Можно отправить');
+  } else {
+    console.log('Нетъ');
+  }
+});
 
 function onDocumentKeydown (evt) {
   if (isEscapeEvt(evt)) {
@@ -20,5 +36,13 @@ function showModal() {
   document.addEventListener('keydown', onDocumentKeydown);
 }
 
+function closeModal() {
+  body.classList.remove('modal-open');
+  imgUploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  imgUploadForm.reset();
+}
+
 imgUploadInput.addEventListener('change', onImgUploadEditing);
+imgUploadCancel.addEventListener('click', closeModal);
 
