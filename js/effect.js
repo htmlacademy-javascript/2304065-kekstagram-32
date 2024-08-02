bab450f86ffdf6fde4db364931d6a883466434f7
Михаил Терешин <medvedstwo@gmail.com>
2024-08-02 21:39:43 +0300

    feat: добавляет функции открытия и скрытия слайдера

--- js/effect.js
+++ js/effect.js
@@ -1,9 +1,39 @@
+
+
 const imgUploadForm = document.querySelector('.img-upload__form');
 const sliderElement = imgUploadForm.querySelector('.effect-level__slider');
+const sliderContainerElement = imgUploadForm.querySelector('.img-upload__effect-level');
 const effectLevelElement = imgUploadForm.querySelector('.effect-level__value');
+const imgElement = imgUploadForm.querySelector('.img-upload__preview img');
+
+let chosenEffect = Effect.DEFAULT;
+
+function isDefault() {
+  return chosenEffect === Effect.DEFAULT;
+}
+
+function setImgStyle() {
+  if(isDefault()) {
+    imgElement.style.filter = null;
+    return;
+  }
+
+  const {value} = effectLevelElement;
+  const {style, unit} = effectToFilter[chosenEffect];
+  imgElement.style.filter = `${style}(${value} ${unit})`;
+}
+
+function showSlider() {
+  sliderContainerElement.classList.remove('.hidden');
+}
+
+function hideSlider() {
+  sliderContainerElement.classList.add('.hidden');
+}
 
 function onSliderUpdate() {
-
+  effectLevelElement.value = sliderElement.noUiSlider.get();
+  setImgStyle();
 }
 
 function createSlider({min, max, step}) {
@@ -23,4 +53,7 @@ function createSlider({min, max, step}) {
   });
 
   sliderElement.noUiSlider.on('update', onSliderUpdate);
+  hideSlider();
 }
+
+
