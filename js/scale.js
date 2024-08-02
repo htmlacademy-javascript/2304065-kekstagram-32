5722f18df0c2025ca2d9ba768c657c84c6bda494
Михаил Терешин <medvedstwo@gmail.com>
2024-08-02 19:54:50 +0300

    feat: добавляет обращение к дом

--- js/scale.js
+++ js/scale.js
@@ -2,3 +2,7 @@ const SCALE_STEP = 25;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 
+const imgUploadForm = document.querySelector('.img-upload__form');
+const buttonScaleSmaller = imgUploadForm.querySelector('.scale__control--smaller');
+const buttonScaleBigger = imgUploadForm.querySelector('.scale__control--bigger');
+const inputScaleValue = imgUploadForm.querySelector('.scale__control--value');
