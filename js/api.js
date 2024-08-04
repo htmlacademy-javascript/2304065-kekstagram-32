ba126ec481d1bcd6ef7cf3ea44732b42ba940749
Михаил Терешин <medvedstwo@gmail.com>
2024-08-04 19:58:16 +0300

    refactor: удаляет импорты и возвращает консоль для проверки

--- js/api.js
+++ js/api.js
@@ -4,6 +4,7 @@ function getData() {
   fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
     .then((response) => response.json())
     .then((photos) => {
+      console.log(photos);
       renderPosts(photos);
     })
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-import { photoDesc } from './data.js';
-import { renderPosts } from './thumbnail.js';
 import './img-upload-form.js';
 import { getData } from './api.js';
 
 getData();
-// renderPosts(photoDesc);
