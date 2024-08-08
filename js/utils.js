d531c5d385eeb2a16358cb6abb4d07f60735d12d
Михаил Терешин <medvedstwo@gmail.com>
2024-08-08 23:11:54 +0300

    feat: добавляет функцию дебаунс

--- js/utils.js
+++ js/utils.js
@@ -25,4 +25,13 @@ function isEscapeEvt (evt) {
   return evt.key === 'Escape';
 }
 
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
 export { createUniqNumber, getRandomInteger, isEscapeEvt};
