3671a41fb689a9d11ff021693b41d2fa24b85d04
Михаил Терешин <medvedstwo@gmail.com>
2024-08-09 00:04:27 +0300

    feat: Добавляет функцию вычисления убывания

--- js/img-filters.js
+++ js/img-filters.js
@@ -1,7 +1,13 @@
+const IMG_RANDOM_COUNT = 10;
+
 const imgFilters = document.querySelector('.img-filters');
 
 function showImgFilters() {
   imgFilters.classList.remove('img-filters--inactive');
+}
+
+function sortCommentsDescending(postA, postB) {
+  return postB.comments.length - postA.comments.length;
 }
 
 export {showImgFilters};
